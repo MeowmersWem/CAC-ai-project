e921834c9af70f8ceb3bf80638098b0d3f0aed51
MeowmersWem <111328584+MeowmersWem@users.noreply.github.com>
2025-10-10 18:37:45 -0500

    defreuhsfejhdn

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   //deployed API URL or localhost for development
-  static const String baseUrl = 'http://localhost:8000/api/v1';
+ static const String baseUrl = 'http://10.0.2.2:8080/api/v1';
   
 
 
